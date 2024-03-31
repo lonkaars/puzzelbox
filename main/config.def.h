4b405fe0f6dbf6c6e377c1b04f20be27d7b7a761
lonkaars <loek@pipeframe.xyz>
2024-03-31 17:01:58 +0200

    add led blink + compile_commands.json for CoC autocomplete

--- main/config.def.h
+++ main/config.def.h
@@ -3,3 +3,6 @@
 #define CONF_NET_SSID "network name"
 #define CONF_NET_PASS "network password"
 
+#include "cyw43_country.h"
+#define CONF_NET_COUNTRY CYW43_COUNTRY_NETHERLANDS
+
--- main/main.cpp
+++ main/main.cpp
@@ -5,13 +5,17 @@
 #include "pico/stdlib.h"
 #include "pico/cyw43_arch.h"
 
+const unsigned int LED_PIN = CYW43_WL_GPIO_LED_PIN;
+
 int main() {
 	stdio_init_all();
+	sleep_ms(2000);
 
-	if (cyw43_arch_init_with_country(CYW43_COUNTRY_UK)) {
+	if (cyw43_arch_init_with_country(CONF_NET_COUNTRY)) {
 		printf("failed to initialize\n");
 		return 1;
 	}
+	cyw43_arch_gpio_put(LED_PIN, 1);
 	printf("initialised\n");
 
 	cyw43_arch_enable_sta_mode();
@@ -21,5 +25,12 @@ int main() {
 		return 1;
 	}
 	printf("connected\n");
+
+	while (true) {
+		cyw43_arch_gpio_put(LED_PIN, 0);
+		sleep_ms(250);
+		cyw43_arch_gpio_put(LED_PIN, 1);
+		sleep_ms(250);
+	}
 }
 
